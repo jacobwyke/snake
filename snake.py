24e12158e0cab1fbd9b344d32bf99a6d040a06db
Jacob Wyke <jacob.wyke@kpmg.co.uk>
2024-03-02 13:51:34 +0000

    Adding in wasd keys

--- snake.py
+++ snake.py
@@ -24,6 +24,10 @@ class Snake:
         pygame.K_RIGHT: RIGHT,
         pygame.K_UP: UP,
         pygame.K_DOWN: DOWN,
+        pygame.K_a: LEFT,
+        pygame.K_d: RIGHT,
+        pygame.K_w: UP,
+        pygame.K_s: DOWN,
     }
 
     def __init__(self):
